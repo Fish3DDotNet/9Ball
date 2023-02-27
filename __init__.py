dfedc7fc678a5f5a7a6481de94242b9b0a7feb49
Jeff <bmffish@gmail.com>
2023-02-27 09:11:50 -0800

    Scoresheet Edit update

--- __init__.py
+++ __init__.py
@@ -70,8 +70,6 @@ def createdb(csvFilename):
     # for team in teams:
     #     db.session.delete(team)
     #     db.session.commit()
-
-
 def convjson(csvFilename):
     # creating a dictionary
     teams = {}
@@ -246,6 +244,227 @@ def update(id):
     else:
         return render_template('update.html', team_to_update=team_to_update)
 
+@app.route('/updaterack', methods=['POST', 'GET'])
+def updaterack():
+
+    if request.method == "POST":
+        for i in range(session['currentrack']):
+            tmpval1 = request.form['p1r'+str(i+1)]
+            tmpval2 = request.form['inr'+str(i+1)]
+            tmpval3 = request.form['dbr'+str(i+1)]
+            tmpval4 = request.form['p2r'+str(i+1)]
+
+            if tmpval1 == '':
+                tmpval1 = 0
+            if tmpval2 == '':
+                tmpval2 = 0
+            if tmpval3 == '':
+                tmpval3 = 0
+            if tmpval4 == '':
+                tmpval4 = 0
+
+            session['p1r' + str(i + 1)] = int(tmpval1)
+            session['inr' + str(i + 1)] = int(tmpval2)
+            session['dbr' + str(i + 1)] = int(tmpval3)
+            session['p2r' + str(i + 1)] = int(tmpval4)
+
+        return redirect('/racks')
+    else:
+        return render_template('eracks.html',
+                               player1=session['player1'],
+                               player2=session['player2'],
+                               player1no=session['player1no'],
+                               player2no=session['player2no'],
+                               team1=session['team1'],
+                               team2=session['team2'],
+                               player1target=session['player1target'],
+                               player2target=session['player2target'],
+                               p1curval=session['p1curval'],
+                               p2curval=session['p2curval'],
+                               p3curval=session['p3curval'],
+                               p1r1=session['p1r1'],
+                               p1r2=session['p1r2'],
+                               p1r3=session['p1r3'],
+                               p1r4=session['p1r4'],
+                               p1r5=session['p1r5'],
+                               p1r6=session['p1r6'],
+                               p1r7=session['p1r7'],
+                               p1r8=session['p1r8'],
+                               p1r9=session['p1r9'],
+                               p1r10=session['p1r10'],
+                               p1r11=session['p1r11'],
+                               p1r12=session['p1r12'],
+                               p1r13=session['p1r13'],
+                               p1r14=session['p1r14'],
+                               p1r15=session['p1r15'],
+                               inr1=session['inr1'],
+                               inr2=session['inr2'],
+                               inr3=session['inr3'],
+                               inr4=session['inr4'],
+                               inr5=session['inr5'],
+                               inr6=session['inr6'],
+                               inr7=session['inr7'],
+                               inr8=session['inr8'],
+                               inr9=session['inr9'],
+                               inr10=session['inr10'],
+                               inr11=session['inr11'],
+                               inr12=session['inr12'],
+                               inr13=session['inr13'],
+                               inr14=session['inr14'],
+                               inr15=session['inr15'],
+                               dbr1=session['dbr1'],
+                               dbr2=session['dbr2'],
+                               dbr3=session['dbr3'],
+                               dbr4=session['dbr4'],
+                               dbr5=session['dbr5'],
+                               dbr6=session['dbr6'],
+                               dbr7=session['dbr7'],
+                               dbr8=session['dbr8'],
+                               dbr9=session['dbr9'],
+                               dbr10=session['dbr10'],
+                               dbr11=session['dbr11'],
+                               dbr12=session['dbr12'],
+                               dbr13=session['dbr13'],
+                               dbr14=session['dbr14'],
+                               dbr15=session['dbr15'],
+                               p2r1=session['p2r1'],
+                               p2r2=session['p2r2'],
+                               p2r3=session['p2r3'],
+                               p2r4=session['p2r4'],
+                               p2r5=session['p2r5'],
+                               p2r6=session['p2r6'],
+                               p2r7=session['p2r7'],
+                               p2r8=session['p2r8'],
+                               p2r9=session['p2r9'],
+                               p2r10=session['p2r10'],
+                               p2r11=session['p2r11'],
+                               p2r12=session['p2r12'],
+                               p2r13=session['p2r13'],
+                               p2r14=session['p2r14'],
+                               p2r15=session['p2r15'],
+                               p1ball1=session['p1ball1'],
+                               p1ball2=session['p1ball2'],
+                               p1ball3=session['p1ball3'],
+                               p1ball4=session['p1ball4'],
+                               p1ball5=session['p1ball5'],
+                               p1ball6=session['p1ball6'],
+                               p1ball7=session['p1ball7'],
+                               p1ball8=session['p1ball8'],
+                               p1ball9=session['p1ball9'],
+                               p1s1=session['p1s1'],
+                               p1s2=session['p1s2'],
+                               p1s3=session['p1s3'],
+                               p1s4=session['p1s4'],
+                               p1s5=session['p1s5'],
+                               p1s6=session['p1s6'],
+                               p1s7=session['p1s7'],
+                               p1s8=session['p1s8'],
+                               p1s9=session['p1s9'],
+                               p1s10=session['p1s10'],
+                               p1s11=session['p1s11'],
+                               p1s12=session['p1s12'],
+                               p1s13=session['p1s13'],
+                               p1s14=session['p1s14'],
+                               p1s15=session['p1s15'],
+                               p1s16=session['p1s16'],
+                               p1s17=session['p1s17'],
+                               p1s18=session['p1s18'],
+                               p1s19=session['p1s19'],
+                               p1s20=session['p1s20'],
+                               p1s21=session['p1s21'],
+                               p1s22=session['p1s22'],
+                               p1s23=session['p1s23'],
+                               p1s24=session['p1s24'],
+                               p1s25=session['p1s25'],
+                               p1s26=session['p1s26'],
+                               p1s27=session['p1s27'],
+                               p1s28=session['p1s28'],
+                               p1s29=session['p1s29'],
+                               p1s30=session['p1s30'],
+                               p1s31=session['p1s31'],
+                               p1s32=session['p1s32'],
+                               p1s33=session['p1s33'],
+                               p1s34=session['p1s34'],
+                               p1s35=session['p1s35'],
+                               p1s36=session['p1s36'],
+                               p1s37=session['p1s37'],
+                               p1s38=session['p1s38'],
+                               p1s39=session['p1s39'],
+                               p1s40=session['p1s40'],
+                               p1s41=session['p1s41'],
+                               p1s42=session['p1s42'],
+                               p1s43=session['p1s43'],
+                               p1s44=session['p1s44'],
+                               p1s45=session['p1s45'],
+                               p1s46=session['p1s46'],
+                               p1s47=session['p1s47'],
+                               p1s48=session['p1s48'],
+                               p1s49=session['p1s49'],
+                               p1s50=session['p1s50'],
+                               p1s51=session['p1s51'],
+                               p1s52=session['p1s52'],
+                               p1s53=session['p1s53'],
+                               p1s54=session['p1s54'],
+                               p1s55=session['p1s55'],
+                               p2s1=session['p2s1'],
+                               p2s2=session['p2s2'],
+                               p2s3=session['p2s3'],
+                               p2s4=session['p2s4'],
+                               p2s5=session['p2s5'],
+                               p2s6=session['p2s6'],
+                               p2s7=session['p2s7'],
+                               p2s8=session['p2s8'],
+                               p2s9=session['p2s9'],
+                               p2s10=session['p2s10'],
+                               p2s11=session['p2s11'],
+                               p2s12=session['p2s12'],
+                               p2s13=session['p2s13'],
+                               p2s14=session['p2s14'],
+                               p2s15=session['p2s15'],
+                               p2s16=session['p2s16'],
+                               p2s17=session['p2s17'],
+                               p2s18=session['p2s18'],
+                               p2s19=session['p2s19'],
+                               p2s20=session['p2s20'],
+                               p2s21=session['p2s21'],
+                               p2s22=session['p2s22'],
+                               p2s23=session['p2s23'],
+                               p2s24=session['p2s24'],
+                               p2s25=session['p2s25'],
+                               p2s26=session['p2s26'],
+                               p2s27=session['p2s27'],
+                               p2s28=session['p2s28'],
+                               p2s29=session['p2s29'],
+                               p2s30=session['p2s30'],
+                               p2s31=session['p2s31'],
+                               p2s32=session['p2s32'],
+                               p2s33=session['p2s33'],
+                               p2s34=session['p2s34'],
+                               p2s35=session['p2s35'],
+                               p2s36=session['p2s36'],
+                               p2s37=session['p2s37'],
+                               p2s38=session['p2s38'],
+                               p2s39=session['p2s39'],
+                               p2s40=session['p2s40'],
+                               p2s41=session['p2s41'],
+                               p2s42=session['p2s42'],
+                               p2s43=session['p2s43'],
+                               p2s44=session['p2s44'],
+                               p2s45=session['p2s45'],
+                               p2s46=session['p2s46'],
+                               p2s47=session['p2s47'],
+                               p2s48=session['p2s48'],
+                               p2s49=session['p2s49'],
+                               p2s50=session['p2s50'],
+                               p2s51=session['p2s51'],
+                               p2s52=session['p2s52'],
+                               p2s53=session['p2s53'],
+                               p2s54=session['p2s54'],
+                               p2s55=session['p2s55'],
+                               submitrackvisible=session['submitrackvisible'],
+                               submitrackinvisible=session['submitrackinvisible']
+                               )
+
 @app.route('/delete/<int:id>', methods=['POST', 'GET'])
 def delete(id):
     team_to_update = teams.query.get_or_404(id)
@@ -261,13 +480,12 @@ def delete(id):
 
 @app.route('/query', methods=['POST', 'GET'])
 def query():
-    names = ["08", "04"]
-    showteams = teams.query.filter(teams.divname=='Tue Double Jeopardy - 442',teams.teamno.in_(names)).all()
+    names = ["05", "10"]
+    showteams = teams.query.filter(teams.divname=='Sat Double Jeopardy - 446',teams.teamno.in_(names)).all()
     # division = teams.query.filter_by(divname = 'Thurs Double Jeopardy - 444').all()
     # showteams = teams.query.filter(division.teamname.in_(names)).all()
 
     return render_template("teams.html", teams=showteams)
-
 
 @app.route('/showteams', methods=['POST', 'GET'])
 def showteams():
@@ -296,7 +514,6 @@ def showteams():
 
         return render_template("teams.html", teams=showteams)
 
-
 @app.route('/reloadteams')
 def reloadteams():
     showteams = teams.query.order_by(teams.date_created)
@@ -310,7 +527,6 @@ def reloadteams():
 
 @app.route('/home')
 def home():
-
 
     return render_template(
         'home.html',
@@ -391,6 +607,34 @@ def home():
         m5p2poi=session['m5p2poi'],
         )
 
+@app.route('/testdata')
+def testdata():
+
+    x = session["currentmatch"]
+
+    curtno1 = '01'
+    curpla1 = 'Jeff Nelson'
+    curplano1 = '13245'
+    curpsl1 = '1'
+
+    curtno2 = '02'
+    curpla2 = 'Jana Nelson'
+    curplano2 = '53421'
+    curpsl2 = 1
+
+    session[str('m' + str(x) + 't1')] = session["team1"] = curtno1
+    session[str('m' + str(x) + 'p1')] = session["player1"] = curpla1
+    session[str('m' + str(x) + 'p1no')] = session["player1no"] = curplano1
+    session[str('m' + str(x) + 'p1sl')] = curpsl1
+    session["player1target"] = str(session["scoretable"]["psl"][str(curpsl1)])
+
+    session[str('m' + str(x) + 't2')] = session["team2"] = curtno2
+    session[str('m' + str(x) + 'p2')] = session["player2"] = curpla2
+    session[str('m' + str(x) + 'p2no')] = session["player2no"] = curplano2
+    session[str('m' + str(x) + 'p2sl')] = curpsl2
+    session["player2target"] = str(session["scoretable"]["psl"][str(curpsl2)])
+
+    return redirect(url_for('home'))
 
 @app.route('/gform')
 def gform():
@@ -697,14 +941,15 @@ def valup():
         session['p1ball' + str(ballval)] = ''
 
     if session['currentrack'] > 1:
-        session['p1total'] = session['p1r' + str(session['currentrack'
-                ])] = session['p1curval'] + session['p1r'
-                + str(session['currentrack'] - 1)]
-        session['p2total'] = session['p2r' + str(session['currentrack'
-                ])] = session['p2curval'] + session['p2r'
-                + str(session['currentrack'] - 1)]
-        session['dbtotal'] = session['dbr' + str(session['currentrack'
-                ])] = session['p3curval']
+        session['p1total'] = \
+            session['p1r' + str(session['currentrack'])] = \
+            session['p1curval'] + session['p1r' + str(session['currentrack'] - 1)]
+        session['p2total'] = \
+            session['p2r' + str(session['currentrack'])] = \
+            session['p2curval'] + session['p2r' + str(session['currentrack'] - 1)]
+        session['dbtotal'] = \
+            session['dbr' + str(session['currentrack'])] = \
+            session['p3curval']
     else:
         session['p1total'] = session['p1r1'] = session['p1curval']
         session['p2total'] = session['p2r1'] = session['p2curval']
